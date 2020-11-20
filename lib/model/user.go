97e9cdcf6e794654b77cfb354a9b635df65b6aee
Nikolaus Ferdy T <nikz.ferdy@gmail.com>
2020-11-20 22:15:16 +0700

    update gomod

--- lib/model/user.go
+++ lib/model/user.go
@@ -3,6 +3,6 @@ package model
 // User Struct is struct to contain user object
 type User struct {
 	ID        int64  `db:"id" json:"id"`
-	FirstName string `db:"first_name json:"first_name"`
-	LastName  string `db:"last_name json:"last_name"`
+	FirstName string `db:"first_name" json:"first_name"`
+	LastName  string `db:"last_name" json:"last_name"`
 }
